b3b9ee4de921570845776e26af5e2febcb926aa2
Rodrigo Medeiros <rodrigovinicius.bsb@gmail.com>
2023-12-06 11:01:30 -0300

    feat: adicionado react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
-    <div className="App">
-     <p>Hello World!!</p>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
-}
-
+};
